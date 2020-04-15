19cfec87d963365fdb6a014181e21ca0647b7ad1
Dimitri Cores <dimi.croes@gmail.com>
2020-04-15 16:48:20 +0200

    Add files via upload

---
+++ src/v1.0/Download.php
@@ -0,0 +1,16 @@
+<?php
+?>
+<!DOCTYPE html>
+<html>
+<body>
+<h2>DONT CHANGE THE NAME OF THE TC FILE</h2>
+    <a href="TC/TC.xml">Download TC file</a><h3>place this in C:\ProgramData\MA Lighting Technologies\grandma\gma2_V_X.X\importexport</h3><br><br>
+    <a href="Macro/Macro.xml">Download Macro file</a><h3>place this in C:\ProgramData\MA Lighting Technologies\grandma\gma2_V_X.X\macros</h3>
+    <form action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_top">
+<input type="hidden" name="cmd" value="_s-xclick" />
+<input type="hidden" name="hosted_button_id" value="T83CF8JRPC6S6" />
+<input type="image" src="https://www.paypalobjects.com/en_US/NL/i/btn/btn_donateCC_LG.gif" border="0" name="submit" title="PayPal - The safer, easier way to pay online!" alt="Donate with PayPal button" />
+<img alt="" border="0" src="https://www.paypal.com/en_NL/i/scr/pixel.gif" width="1" height="1" />
+</form>
+
+</html>
